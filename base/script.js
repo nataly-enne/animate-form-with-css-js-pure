e5af18de0fd10aac15c531149ba63e6ca259c05c
nataly-enne <ennenataly@gmail.com>
2020-03-29 15:47:03 -0300

    fix: resolvendo bug de rolagem ao remover form enquanto sai da tela

--- base/script.js
+++ base/script.js
@@ -5,3 +5,17 @@ btnLogin.addEventListener("click", event => {
     event.preventDefault();
     form.classList.add("form-hide");
 });
+
+form.addEventListener("animationstart", event =>{
+    if(event.animationName == "move-down"){
+        document.querySelector("body").style.overflow = "hidden";
+    }
+});
+
+form.addEventListener("animationend", event => {
+    if(event.animationName == "move-down"){
+    form.style.display = "none";
+    document.querySelector("body").style.overflow = "none";
+
+    }
+});
