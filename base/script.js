639ba96621821ba8766ba58aace9ea3d872da3af
nataly-enne <ennenataly@gmail.com>
2020-03-29 16:36:57 -0300

    create: iniciando animação dos quadrados animados na tela

--- base/script.js
+++ base/script.js
@@ -35,3 +35,25 @@ form.addEventListener("animationend", event => {
     document.querySelector("body").style.overflow = "none";
     }
 });
+
+/* background squares */
+
+const ulSquares = document.querySelector("ul.squares");
+
+for (let i = 0; i < 11; i++){
+    const li = document.createElement("li");
+
+    const random = (min, max) => Math.random() * (max - min) + min;
+
+    const size = Math.floor(random(10, 120));
+
+    const position = random(1, 99);
+
+    li.style.width = `${size}px`;
+    li.style.height = `${size}px`;
+    li.style.bottom = `-${size}px`;
+
+        li.style.left = `${position}%`
+
+    ulSquares.appendChild(li);
+}
